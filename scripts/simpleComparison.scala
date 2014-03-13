e9e1f42169dd831b9099990caf2ca289a10c3a6b
xrrocha <ricardo@plenix.com>
2014-03-13 13:15:39 -0400

    Adding section on data structures

---
+++ scripts/simpleComparison.scala
@@ -0,0 +1,15 @@
+val typo = "accet"
+
+val relatedWords = Seq(
+  "academic",
+  "academy",
+  "accent",
+  "accept",
+  "accident",
+  "account",
+  "accountant",
+  "acid")
+
+val levenshtein = new org.apache.lucene.search.spell.LevensteinDistance
+
+relatedWords.foreach(w => println(f"$w ${levenshtein.getDistance(w, typo)}%.4f"))
