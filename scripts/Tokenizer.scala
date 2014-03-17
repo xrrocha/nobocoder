e0d70775597a13ecc92ee86ebcc53eda84d0b8d1
xrrocha <ricardo@plenix.com>
2014-03-17 10:58:10 -0400

    Adding notes on traits, objects and classes

---
+++ scripts/Tokenizer.scala
@@ -0,0 +1,17 @@
+import com.typesafe.scalalogging.slf4j.Logging
+
+trait Tokenizer {
+  def tokenize(string: String): Seq[String]
+}
+
+class DefaultTokenizer(separator: String) extends Tokenizer {
+  def tokenize(string: String) = string.split(separator)
+}
+
+object Tokenizer extends Logging {
+  def apply(): Tokenizer = apply("\\s")
+  def apply(separator: String): Tokenizer = new DefaultTokenizer(separator)
+}
+
+val tokenizer = Tokenizer(",")
+println(tokenizer.tokenize("a,b,c")) // prints: WrappedArray(a, b, c)
